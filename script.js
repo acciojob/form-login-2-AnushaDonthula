33415fb85fd54bcf4c6cf6a19d18ff63c4407e0c
AnushaDonthula <162703733+AnushaDonthula@users.noreply.github.com>
2024-08-01 22:58:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 //your JS code here. If required.
-function displayFormData() {
-    // Get form data
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const phoneNumber = document.getElementById('phoneNumber').value;
-    const emailId = document.getElementById('emailId').value;
-
-    // Format the alert message
-    const alertMessage = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
-
-    // Display the alert
-    alert(alertMessage);
-}
